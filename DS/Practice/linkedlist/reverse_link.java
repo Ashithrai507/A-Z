e176c4df84b2aa1ec93f2ca7836ed70af755dd1b
Ashith Rai <ashithgit@gmail.com>
2026-03-30 22:39:43 +0530

    LL enhace

---
+++ DS/Practice/linkedlist/reverse_link.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class reverse_link {
+    public static void main(String[] ashith){
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+        int left =0;
+        int right=list.size()-1;
+
+        while(left<right){
+            int temp = list(left);
+            list(left)
+
+        }
+    }
+}
